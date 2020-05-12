e69e2e450d1cf664a923984d46609d4d0b6e972a
tran.minh <tran.minh@jvb-corp.com>
2020-05-12 14:26:21 +0700

    add some feature

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 $(document).ready(function () {
-    const WHEEL_PROTO = new WHEEL($('#wheel'), '10');
-    // const SUB_WHEEL = new WHEEL($('#sub_wheel'), '12');
+    const TRIGGER_DOM = $('#submit');
+    generateWheel(['left', 'right', 'straight'], false);
 
-    WHEEL_PROTO.dom.height(WHEEL_PROTO.dom.width());
-    // SUB_WHEEL.dom.height(SUB_WHEEL.dom.width());
-
-    WHEEL_PROTO.dom.on('click', function () {
-        const f = getRandomInt(1, 360);
-        WHEEL_PROTO.spin(f);
-        // SUB_WHEEL.spin(f);
+    $('#option, #sub_option').on('blur', function () {
+        TRIGGER_DOM.trigger('submit');
     });
 
-    //test($('#wheel')[0]);
+    $('#sub_enable').on('change', function () {
+        TRIGGER_DOM.trigger('submit');
+    });
+
+    TRIGGER_DOM.on('submit', function () {
+        const input = $('#option').val().split('\n');
+        generateWheel(input, !!$('#sub_enable').prop('checked'));
+    })
 });
 
-const WHEEL = function (dom, duration) {
-    this.constructor(dom, duration);
+const WHEEL = function (dom, duration, input) {
+    this.constructor(dom, duration, input);
 };
 
 WHEEL.prototype = {
-    dom: $('#wheel'),
+    dom: '',
     status: true,
     duration: '0.5s',
     currentDeg: 0,
     radius: 0,
     x_cor: 0,
     y_cor: 0,
-    num: 20,
+    num: 3,
+    input: [],
 
-    constructor: function (dom, duration) {
+    constructor: function (dom, duration, input) {
         this.dom = dom;
         this.duration = duration;
         this.dom.on('transitionend', function () {
@@ -37,8 +40,13 @@ WHEEL.prototype = {
         }.bind(this));
         this.x_cor = this.y_cor = dom.attr('width') / 2;
         this.radius = dom.attr('width') / 2;
-        this.angle = 360 / this.num;
+        this.calculateInput(input);
         this.drawCircle();
+    },
+    calculateInput: function (input) {
+        this.num = input.length;
+        this.angle = 360 / this.num;
+        this.input = input;
     },
     spin: function (f) {
         if (this.status) {
@@ -56,29 +64,47 @@ WHEEL.prototype = {
     getRandomDeg: function (f) {
         return 360 * 10 + f + getRandomInt(1, 20) * this.angle;
     },
+    drawAngledLine: function (x, y, length, angle, input) {
+        const ctx = this.dom[0].getContext('2d');
+
+        if (typeof input !== 'string') {
+            const base_image = new Image();
+            base_image.src = `./img/fff&text=${getRandomInt(1, 9)}.png`;
+
+            base_image.onload = function () {
+                const radians = this.convertDegToRadian(angle);
+                ctx.beginPath();
+                ctx.fillStyle = ctx.createPattern(base_image, 'repeat');
+                ctx.moveTo(x, y);
+                ctx.arc(this.x_cor, this.y_cor, this.radius, radians, this.convertDegToRadian(angle + this.angle));
+                ctx.fill();
+            }.bind(this);
+        } else {
+            const radians = this.convertDegToRadian(angle);
+            ctx.beginPath();
+            ctx.fillStyle = getRandomColor();
+
+            ctx.moveTo(x, y);
+            ctx.arc(this.x_cor, this.y_cor, this.radius, radians, this.convertDegToRadian(angle + this.angle));
+            ctx.fill();
+
+            // fill text
+            ctx.save();
+            ctx.translate(this.x_cor, this.y_cor);
+            ctx.rotate(this.convertDegToRadian(angle + this.angle / 2));
+            ctx.textAlign = "center";
+            ctx.fillStyle = "#fff";
+            ctx.font = 'bold 30px sans-serif';
+            ctx.fillText(input, this.radius - 100, 0);
+            ctx.restore();
+        }
+    },
     drawCircle: function () {
         const can = this.dom[0];
         if (can.getContext) {
-            const ctx = can.getContext('2d');
-
-            const drawAngledLine = function (x, y, length, angle) {
-
-                const base_image = new Image(50, 50);
-                base_image.src = `./img/fff&text=${getRandomInt(1, 9)}.png`;
-
-                base_image.onload = function () {
-                    const radians = this.convertDegToRadian(angle);
-                    ctx.beginPath();
-                    ctx.fillStyle = ctx.createPattern(base_image, 'repeat');
-                    ctx.moveTo(x, y);
-                    ctx.arc(this.x_cor, this.y_cor, this.radius, radians, this.convertDegToRadian(angle + this.angle));
-                    ctx.fill();
-                }.bind(this);
-
-            }.bind(this);
-
-            for (let i = 0; i < 1; ++i) {
-                drawAngledLine(this.x_cor, this.y_cor, this.radius, this.angle * i);
+            for (let i = 0; i < this.num; ++i) {
+                console.log(this.input[i]);
+                this.drawAngledLine(this.x_cor, this.y_cor, this.radius, this.angle * i, this.input[i]);
             }
 
         } else {
@@ -118,4 +144,24 @@ function getRandomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+function generateWheel(input, subWheelEnabled) {
+    const WHEEL_PROTO = new WHEEL($('#wheel'), '10', input);
+    WHEEL_PROTO.dom.height(WHEEL_PROTO.dom.width());
+
+    let SUB_WHEEL;
+    if (subWheelEnabled) {
+        const subInput = $('#sub_option').val().split('\n');
+        SUB_WHEEL = new WHEEL($('#sub_wheel'), '12', subInput);
+        SUB_WHEEL.dom.height(SUB_WHEEL.dom.width());
+    }
+
+    WHEEL_PROTO.dom.on('click', function () {
+        const f = getRandomInt(1, 360);
+        WHEEL_PROTO.spin(f);
+        if (subWheelEnabled) {
+            SUB_WHEEL.spin(f);
+        }
+    });
 }
